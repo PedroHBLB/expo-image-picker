400127dd4b46f9279e328cd2a23aeb4565930e1c
Pedro Henrique Barbosa Leite Barbieri <85046306+PedroHBLB@users.noreply.github.com>
2022-11-08 14:42:54 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import { View, TouchableOpacity, Text, StyleSheet, Image } from "react-native";
+
+import Constants from "expo-constants";
+import * as Permissions from "expo-permissions";
+import * as ImagePicker from "expo-image-picker";
+import Axios from "axios";
+import api from './config/configApi';
+
+export default function Upload() {
+  const [avatar, setAvatar] = useState();
+
+  async function imagePickerCall() {
+    if (Constants.platform.ios) {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+
+      if (status !== "granted") {
+        alert("Nós precisamos dessa permissão.");
+        return;
+      }
+    }
+
+    const data = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All
+    });
+
+    if (data.cancelled) {
+      return;
+    }
+
+    if (!data.uri) {
+      return;
+    }
+
+    setAvatar(data);
+  }
+
+  async function uploadImage() {
+    const path = avatar.uri.split('/');
+    const fileName = path[path.length - 1];
+    const data = new FormData();
+
+    data.append("avatar", {
+      uri: avatar.uri,
+      type: avatar.type,
+      name: fileName,
+    });
+    const headers = {
+      'headers': {
+        'Content-Type': 'application/json'
+      }
+    }
+    console.log(data);
+
+    await api.post("/uploadFiles", data, headers)
+    .then((response) => {
+      console.log(response);
+      console.log("Passei!");
+    }).catch((error) => {
+      console.log(error);
+      console.log("Não passei!");
+    })
+  }
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{
+          uri: avatar
+            ? avatar.uri
+            : "https://images.unsplash.com/photo-1612810806695-30f7a8258391?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+        }}
+        style={styles.avatar}
+      />
+      <TouchableOpacity style={styles.button} onPress={imagePickerCall}>
+        <Text style={styles.buttonText}>Escolher imagem</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={uploadImage}>
+        <Text style={styles.buttonText}>Enviar imagem</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  button: {
+    width: 150,
+    height: 50,
+    borderRadius: 3,
+    backgroundColor: "#7159c1",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20
+  },
+  buttonText: {
+    color: "#fff"
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 50
+  }
+});
